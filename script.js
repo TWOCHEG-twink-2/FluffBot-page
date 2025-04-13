d7c691e2662a5f44ae5b3b7ec1d922302118d37b
TWOCHEG <150810031+TWOCHEG@users.noreply.github.com>
2025-04-13 17:22:06 +0300

    ставлю джаваскрипту 4/10 с нейронкой было еще более менее

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', function () {
     const folderPath = 'texts';
     const files = ['main.md', 'privacy_policy.md'];
     const contentDiv = document.getElementById('content');
-
     for (let file of files) {
         fetch(`${folderPath}/${file}`)
             .then(response => response.text())
@@ -17,13 +16,13 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Добавляем футер
     fetch('templates/footer.html')
-    .then(response => response.text())
-    .then(data => {
-        const footerContainer = document.createElement('div');
-        footerContainer.className = 'footer';
-        footerContainer.innerHTML = data;
-        document.body.appendChild(footerContainer);
-    });
+        .then(response => response.text())
+        .then(data => {
+            const footerContainer = document.createElement('div');
+            footerContainer.className = 'footer';
+            footerContainer.innerHTML = data;
+            document.body.appendChild(footerContainer);
+        });
     // загрузка кота строителя
     fetch('templates/cat_builder.html')
     .then(response => response.text())
@@ -32,5 +31,35 @@ document.addEventListener('DOMContentLoaded', function () {
         floatContainer.className = 'float-image';
         floatContainer.innerHTML = data;
         document.body.appendChild(floatContainer);
+
+        // список звуков
+        const sounds = [
+            'sound/hitt-1.mp3',
+            'sound/hitt-2.mp3',
+            'sound/meow-1.mp3',
+            'sound/meow-2.mp3',
+            'sound/meow-3.mp3',
+            'sound/meow-4.mp3',
+            'sound/purreow-1.mp3',
+            'sound/purreow-2.mp3'
+        ];
+
+        // удаление кота строителя на третий клик и проигрывание звука
+        const bubbleImage = document.querySelector('.bubble-image');
+        const bubbleFooter = document.querySelector('.footer-bubble');
+        let clickCount = 0;
+
+        bubbleImage.addEventListener('click', () => {
+            clickCount++;
+            
+            // проигрывание случайного звука
+            const randomSound = sounds[Math.floor(Math.random() * sounds.length)];
+            const audio = new Audio(randomSound);
+            audio.play();
+
+            if (clickCount >= 3) {
+                bubbleFooter.classList.add('hidden');
+            }
+        });
     });
 });
